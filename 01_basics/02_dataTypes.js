6150bed68e3c4ce1ae2572ab85b47216306fc184
Omkar Dongare <omkardongare951@gmail.com>
2026-01-07 10:04:25 +0000

    datatypes commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use Strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3)  // code readability should be high
+
+console.log("omkar")
+
+
+let name = "omkar"
+let age = 21
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // object
